39cf16c04c4f4478b630f7234b583568d8fa67d1
Muhammad Khan <muhammadkhan08561@gmail.com>
2026-07-10 15:23:54 +0500

    adding other agent files

---
+++ agents/base.py
@@ -0,0 +1,50 @@
+﻿from __future__ import annotations
+
+import logging
+
+from livekit.agents import Agent, RunContext
+
+from userData import UserData
+
+logger = logging.getLogger("restaurant-example")
+
+RunContext_T = RunContext[UserData]
+
+
+class BaseAgent(Agent):
+    async def on_enter(self) -> None:
+        agent_name = self.__class__.__name__
+        logger.info(f"entering task {agent_name}")
+
+        userdata: UserData = self.session.userdata
+        chat_ctx = self.chat_ctx.copy()
+
+        # add the previous agent's chat history to the current agent
+        if isinstance(userdata.prev_agent, Agent):
+            truncated_chat_ctx = userdata.prev_agent.chat_ctx.copy(
+                exclude_instructions=True,
+                exclude_function_call=False,
+                exclude_handoff=True,
+                exclude_config_update=True,
+            ).truncate(max_items=6)
+            existing_ids = {item.id for item in chat_ctx.items}
+            items_copy = [
+                item for item in truncated_chat_ctx.items if item.id not in existing_ids
+            ]
+            chat_ctx.items.extend(items_copy)
+
+        # add an instructions including the user data as assistant message
+        chat_ctx.add_message(
+            role="system",  # role=system works for OpenAI's LLM and Realtime API
+            content=f"You are {agent_name} agent. Current user data is {userdata.summarize()}",
+        )
+        await self.update_chat_ctx(chat_ctx)
+        self.session.generate_reply(tool_choice="none")
+
+    async def _transfer_to_agent(self, name: str, context: RunContext_T) -> tuple[Agent, str]:
+        userdata = context.userdata
+        current_agent = context.session.current_agent
+        next_agent = userdata.agents[name]
+        userdata.prev_agent = current_agent
+
+        return next_agent, f"Transferring to {name}."
